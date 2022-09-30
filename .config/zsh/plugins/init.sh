fc3be14df24b92ae03e4ae9b096f41801da1e355
jared-lbx <jjewell@lunchbox.io>
2022-09-30 17:10:54 -0600

    update plugin locations

--- .config/zsh/plugins/init.sh
+++ .config/zsh/plugins/init.sh
@@ -1,10 +1,10 @@
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.config/zsh/plugins/fsh/fast-syntax-highlighting.plugin.zsh
-source ~/.config/zsh/plugins/nvm.sh
+source ~/.config/zsh/plugins/zsh-nvm/zsh-nvm.plugin.zsh
 source ~/.config/zsh/plugins/jump.sh
 source ~/.config/zsh/plugins/jump.sh
 source ~/.config/zsh/plugins/starship.sh
 source ~/.config/zsh/plugins/fzf.sh
 source ~/.config/zsh/plugins/fzf-fn.sh
 source ~/.config/zsh/plugins/base16.sh
-source ~/zsh-plugins/fzf-tab/fzf-tab.plugin.zsh
+source ~/.config/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh
